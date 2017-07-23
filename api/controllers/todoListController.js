38014c9e87b67c15d1ce3704d8b4699d59aaff85
linkhui <lihui_shanghai@hotmail.com>
2017-07-23 13:19:33 +0800

    读取参数

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -1,14 +1,20 @@
 'use strict';
 
+// var url = require("url");
+
 exports.test = function (req, res) {
     console.log('call test');
+    // var pathname = url.parse(req.url).pathname;
+    // var querys = url.parse(req.url).query;
+    var city = req.query['city'];
+    console.log("Request for "  + " " + city + " received.");
 
     res.json({
     "success": true,
         "errorMsg": "成功",
         "errorCode": "200",
         "fields": {
-        "城市": "上海",
+        "城市": city,
             "最低温度": "36℃",
             "最高温度": "42℃",
             "天气": "多云"
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT | 3000;
-    // bodyParser = require('body-parser');
+    bodyParser = require('body-parser');
 
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var routes = require('./api/routes/todoListRoutes');
 routes(app);
